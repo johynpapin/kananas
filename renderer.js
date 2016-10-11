6f3dbad1d88459f21c248c9da66e84e919cea559
Papin Johyn <johynpapin@kiwiasso.org>
2016-10-11 16:47:07 +0200

    Remove library Wanakana

--- renderer.js
+++ renderer.js
@@ -1,23 +1,41 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// Main window code, this is executed in the browser frame
 
-const wanakana = require('wanakana');
+//const wanakana = require('wanakana');
 
-let randomProperty = function (obj) {
+let hiraganas = ["あ", "い", "う", "え", "お",
+                 "か", "き", "く", "け", "こ",
+                 "さ", "し", "す", "せ", "そ",
+                 "た", "ち", "つ", "て", "と",
+                 "な", "に", "ぬ", "ね", "の",
+                 "は", "ひ", "ふ", "へ", "ほ",
+                 "ま", "み", "む", "め", "も",
+                 "や",       "ゆ",      "よ",
+                 "ら", "り", "る", "れ", "ろ",
+                 "わ",                  "を",
+                 "ん"];
+
+/*let randomProperty = function (obj) {
     var keys = Object.keys(obj);
     return obj[keys[ keys.length * Math.random() << 0]];
-};
+};*/
+
+pickRandomKana() {
+    return hiraganas[hiraganas.length * Math.random() << 0] // << 0 to make result a integer
+}
+
+isKana(kana) {
+    return hiriganas.indexOf(kana) != -1;
+}
 
 let kana = document.getElementById('kana');
 let input = document.getElementById('romaji-input');
-let current = kana.innerText = randomProperty(wanakana.R_to_J);
+let current = kana.innerText = pickRandomKana();
 
 input.addEventListener('keypress', (e) => {
     if(e.keyCode === 13) {
-        if(e.target.value === wanakana.J_to_R[current]) {
+        if(isKana(e.target.value)) {
             kana.classList.remove('appear');
-            kana.innerHTML = current = randomProperty(wanakana.R_to_J);
+            kana.innerHTML = current = pickRandomKana();
             void kana.offsetWidth;
             kana.classList.add('appear');
         } else {
